e347379f8a24fe4dd7edeed26f2e86886d74f381
bhar732 <funspeed97@gmail.com>
2017-12-29 14:12:21 +0530

    Add files via upload

---
+++ encrypto.m
@@ -0,0 +1,32 @@
+fin = fopen('complaint.txt', 'rb');    %opening text file
+Str = fread(fin, [1, inf], 'char'); %reading text file
+fclose(fin);        %closing the file
+Str=uint16(Str);    %converting to 16 bit numbers for proper calculation
+
+x=dicomread('a7.dcm');   %reading the image file
+x=uint16(x);                %conversion to 16 bit
+[x_row,x_col]=size(x);
+
+c=numel(Str);   %counting characters
+a=1;
+
+%encrypting loop
+for i=1:x_row
+    for j=1:x_col
+        if(a<=c)
+            
+            if(x(i,j)+Str(a)>255)
+                temp=x(i,j)+Str(a)-256;
+            else
+                temp=x(i,j)+Str(a);
+            end
+            z(i,j)=uint8(temp);
+        else
+            z(i,j)=uint8(x(i,j));
+        end
+        a=a+1;
+    end
+end
+
+imwrite(z,'encrypted.png');     %writing the encrypted data as pixels in image
+
